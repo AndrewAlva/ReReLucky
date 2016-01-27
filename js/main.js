ad82156b88a331b8e637e4b3c7e654d024dce06c
Lucky Arte <vanhuten@Vanhuten.local>
2016-01-26 19:25:19 -0600

    First round Feedback, art rejected, last file with lucky 3.0

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 jQuery(document).ready(function($) {
 
 	// SET UI COLOURS ON TIME
-		var today = new Date();
-		var todayHour = today.getHours();
-		var todayMinutes = today.getMinutes();
+		// var today = new Date();
+		// var todayHour = today.getHours();
+		// var todayMinutes = today.getMinutes();
 		
-		// var todayHour = 19;
-		// var todayMinutes = 31;
+		// // var todayHour = 19;
+		// // var todayMinutes = 31;
 
-		var luckyBody = document.getElementById("luckyBody");
-		console.log("What time is it in here? " + todayHour + ":" + todayMinutes);
+		// var luckyBody = document.getElementById("luckyBody");
+		// console.log("What time is it in here? " + todayHour + ":" + todayMinutes);
 
-		// NITE STYLE // LUCKY IS CLOSED
-		if(todayHour >= 0 && todayHour < 9 || todayHour >= 19 && todayHour < 24){
-			luckyBody.classList.add("niteTimeFontColor");
-			luckyBody.style.backgroundColor = "#0c1f28";
-		}
+		// // NITE STYLE // LUCKY IS CLOSED
+		// if(todayHour >= 0 && todayHour < 9 || todayHour >= 19 && todayHour < 24){
+		// 	luckyBody.classList.add("niteTimeFontColor");
+		// 	luckyBody.style.backgroundColor = "#0c1f28";
+		// }
 		
-		// GRAY STYLE // 9AM - 11AM && 4PM - 7PM
-		if(todayHour >= 9 && todayHour < 11 || todayHour >= 16 && todayHour < 19){
-			luckyBody.classList.add("dayTimeFontColor");
-			luckyBody.style.backgroundColor = "#d7dbe3";
-		}
+		// // GRAY STYLE // 9AM - 11AM && 4PM - 7PM
+		// if(todayHour >= 9 && todayHour < 11 || todayHour >= 16 && todayHour < 19){
+		// 	luckyBody.classList.add("dayTimeFontColor");
+		// 	luckyBody.style.backgroundColor = "#d7dbe3";
+		// }
 		
-		// DAY STYLE // LUCKY IS OPEN
-		if(todayHour >= 11 && todayHour < 16){
-			luckyBody.classList.add("dayTimeFontColor");
-			luckyBody.style.backgroundColor = "#edf1fa";
-		}
+		// // DAY STYLE // LUCKY IS OPEN
+		// if(todayHour >= 11 && todayHour < 16){
+		// 	luckyBody.classList.add("dayTimeFontColor");
+		// 	luckyBody.style.backgroundColor = "#edf1fa";
+		// }
 	// END COLOURS ON TIME
 
 
